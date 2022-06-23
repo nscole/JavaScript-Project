f66355d29ded583fa9d1498b8008b376613cfdeb
Naomi Cole <naomisarahcole@gmail.com>
2022-06-23 12:55:40 +1000

    create function to load and show question

---
+++ script.js
@@ -0,0 +1,20 @@
+// https://opentdb.com/api.php?amount=10
+
+const _question = document.getElementById("question");
+const _options = document.querySelector(".quiz-options")
+
+
+async function loadQuestion () {
+    const apiURL = 'https://opentdb.com/api.php?amount=10';
+    const results = await fetch(`${apiURL}`);
+    const data = await results.json();
+    // console.log(data.results[0]);
+    showQuestion(data.results[0])
+}
+
+function showQuestion(data) {
+    let correctAnswer = data.correct_answerl
+    console.log(data);
+}
+
+loadQuestion();
